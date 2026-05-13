60808a8190de3ba05c3a95fb56d7fede0cec6a28
Arthur Kunz <arthurkunz@Mac.fritz.box>
2026-05-13 20:59:59 +0200

    style(SignInForm): did something wrong and had to do it again

--- features/auth/components/SignInForm.tsx
+++ features/auth/components/SignInForm.tsx
@@ -85,34 +85,34 @@ export default function SignInForm({ onSuccess, onGoToSignUp }: SignInProps) {
   }
 
   return (
-    <div className='w-full h-full flex flex-col items-center gap-20'>
+    <div className='w-full flex flex-col items-center gap-15'>
 
       <div className='w-full h-full flex flex-col items-center gap-2.5'>
-        <h1 className='text-4xl font-bold text-lighttext-heading'>Login</h1>
-        <span className='text-center text-sm text-lighttext-subheading'>Log dich ein und lerne <span className='text-brand text-sm font-semibold '>Leipzig</span> kennen</span>
+        <h1 className='text-4xl font-bold text-light-heading'>Login</h1>
+        <span className='text-center text-sm text-light-subheading'>Log dich ein und lerne <span className='text-brand text-sm font-semibold '>Leipzig</span> kennen</span>
       </div>
 
       <form className='w-full flex flex-col gap-12.5' onSubmit={handleSignIn} noValidate>
         <div className='w-full'>
           <div className='w-full flex flex-col gap-5'>
             <div className='w-full flex flex-col gap-2'>
-              <label className='text-sm text-lighttext-label'>Email</label>
+              <label className='text-sm text-light-label'>Email</label>
               <input
               type='email'
               placeholder='max.mustermann@example.com'
-              className='w-full px-3 py-4 bg-graybg rounded-md text-sm text-lighttext focus:outline-none placeholder:text-xs placeholder:text-lighttext-placeholder'
+              className='w-full px-3 py-4 bg-input-bg rounded-md text-sm text-light focus:outline-none placeholder:text-xs placeholder:text-light-placeholder'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className='w-full flex flex-col gap-2'>
-              <label className='text-sm text-lighttext-label'>Passwort</label>
+              <label className='text-sm text-light-label'>Passwort</label>
               <input
               type='password'
               placeholder='Gib dein Passwort ein'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className='w-full px-3 py-4 bg-graybg rounded-md text-sm text-lighttext focus:outline-none placeholder:text-xs placeholder:text-lighttext-placeholder'
+              className='w-full px-3 py-4 bg-input-bg rounded-md text-sm text-light focus:outline-none placeholder:text-xs placeholder:text-light-placeholder'
               />
             </div>
           </div>
@@ -121,27 +121,25 @@ export default function SignInForm({ onSuccess, onGoToSignUp }: SignInProps) {
         <div className='w-full flex flex-col gap-5'>
           <button 
             type='submit'
-            className='flex w-full items-center justify-center gap-2 rounded-full bg-whitebg py-3 text-sm font-semibold'
+            className='flex w-full items-center justify-center gap-2 rounded-full bg-button-bg py-3 text-sm font-semibold'
           >
-            Login
+            Sign Up
           </button>
           <div className='w-full flex justify-between items-center'>
-            <div className='w-full h-0.25 bg-lightgraybg'></div>
-            <span className='text-xs text-lighttext-muted px-2.5'>Oder</span>
-            <div className='w-full h-0.25 bg-lightgraybg'></div>
+            <div className='w-full h-0.25 bg-divider-bg'></div>
+            <span className='text-xs text-light-muted px-2.5'>Oder</span>
+            <div className='w-full h-0.25 bg-divider-bg'></div>
           </div>
           <button
             type='button'
             onClick={handleGoogleSignIn}
-            className='flex w-full items-center justify-center gap-2 rounded-full bg-whitebg py-3 text-sm font-semibold'
+            className='flex w-full items-center justify-center gap-2 rounded-full bg-button-bg py-3 text-sm font-semibold'
           >
             <Image src='/icons/Google.png' alt='' width={18} height={18} className='shrink-0' />
             Google
           </button>
         </div>
       </form>
-
-      <span className='text-xs text-lighttext-muted'>Du hast noch kein Account? <span className='text-brand text-sm font-semibold cursor-pointer' onClick={onGoToSignUp}>Sign Up</span></span>
     </div>
   )
 }
