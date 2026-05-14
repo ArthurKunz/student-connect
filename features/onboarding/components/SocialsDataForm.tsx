ad35b980ef8c6a1328a34f56a59237d8696136ac
Arthur Kunz <arthurkunz@Mac.fritz.box>
2026-05-14 17:12:07 +0200

    style(SocialsDataForm): styled the SocialsDataForm

--- features/onboarding/components/SocialsDataForm.tsx
+++ features/onboarding/components/SocialsDataForm.tsx
@@ -15,12 +15,52 @@ export default function SocialsDataForm ({ onSuccess, onGoBack }: SocialsDataPro
     }
 
     return (
-        <div className='flex flex-col gap-4'>
-            <input type="text" placeholder="Instagram" value={instagram} className="p-2 border" step="0.1" onChange={(e) => setInstagram(e.target.value)}/>
-            <input type="text" placeholder="Tiktok" value={tiktok} className="p-2 border" step="0.1" onChange={(e) => setTiktok(e.target.value)}/>
-            <input type="text" placeholder="Snapchat" value={snapchat} className="p-2 border" step="0.1" onChange={(e) => setSnapchat(e.target.value)}/>
-            <button className="bg-blue-500 text-white p-2 rounded" onClick={() => onSuccess(data)}>Save & Continue</button>
-            <button className="bg-blue-500 text-white p-2 rounded" onClick={onGoBack}>back</button>
+        <div className='w-full flex flex-col items-center gap-15'>
+            <div className='w-full h-full flex flex-col items-center gap-2.5'>
+                <h1 className='text-4xl font-bold text-light-heading'>Profil erstellen</h1>
+                <span className='text-center text-sm text-light-subheading'>Gib deine Social Media Accounts an</span>
+            </div>
+            <div className='w-full flex flex-col gap-7.5'>
+                <div className='w-full flex flex-col gap-2'>
+                    <label className='text-xs text-light-label'>Vorname</label>
+                    <input 
+                    type="text" 
+                    placeholder="Instagram" 
+                    value={instagram} 
+                    className="w-full text-light-input text-xs px-3 h-12 bg-input-bg border border-input-border rounded-md text-sm text-light focus:outline-none placeholder:text-xs placeholder:text-light-placeholder" 
+                    onChange={(e) => setInstagram(e.target.value)} 
+                    required 
+                    />
+                </div>
+                <div className='w-full flex flex-col gap-2'>
+                    <label className='text-xs text-light-label'>Snapchat</label>
+                    <input 
+                    type="text" 
+                    placeholder="Snapchat" 
+                    value={snapchat} 
+                    className="w-full text-light-input text-xs px-3 h-12 bg-input-bg border border-input-border rounded-md text-sm text-light focus:outline-none placeholder:text-xs placeholder:text-light-placeholder" 
+                    onChange={(e) => setSnapchat(e.target.value)} 
+                    required 
+                    />
+                </div>
+                <div className='w-full flex flex-col gap-2'>
+                    <label className='text-xs text-light-label'>TikTok</label>
+                    <input 
+                    type="text" 
+                    placeholder="TikTok" 
+                    value={tiktok} 
+                    className="w-full text-light-input text-xs px-3 h-12 bg-input-bg border border-input-border rounded-md text-sm text-light focus:outline-none placeholder:text-xs placeholder:text-light-placeholder" 
+                    onChange={(e) => setTiktok(e.target.value)} 
+                    required 
+                    />
+                </div>
+            </div>
+            <button 
+            onClick={() => onSuccess(data)}
+            className='flex w-full items-center justify-center gap-2 rounded-full bg-button-bg py-3 text-sm font-semibold'
+            >
+            Weiter
+            </button>
         </div>
     )
 }
